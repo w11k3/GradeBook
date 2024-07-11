0c904152699a82d844978606ce1e8f4c752555dd
PauloKinjo <paulo@perfectstorm.gg>
2024-07-11 18:56:51 -0400

    Student Grades

---
+++ GradeBookTest.cs
@@ -0,0 +1,12 @@
+﻿public class GradeBookTest
+{
+    public static void Main(string[] args)
+    {
+        // one-dimensional array of student grades
+        int[] gradesArray = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+
+        GradeBook myGradeBook = new GradeBook("CS101 Introduction to C# Programming", gradesArray);
+        myGradeBook.DisplayMessage();
+        myGradeBook.ProcessGrades();
+    } // end Main
+} // end class GradeBookTest
